e876aa48a6ae0272b121db62baad7983b7994918
Raimondo Caraviello <raicaraviello2@gmail.com>
2018-02-19 12:18:49 +0100

    ottimizzato somma4 esomma10

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -4,18 +4,14 @@ public class Sommatore
 
 	public static int somma4(int x) 
 	{
-		for (int i = 0; i < 4; i++) 
 		
-			x++;
-			return x;
+			return x+4;
 		
 	}
 	public static int somma10(int x) 
 	{
-		for (int i = 0; i < 10; i++) 
 		
-			x++;
-			return x;
+			return x+10;
 		
 	}
 
