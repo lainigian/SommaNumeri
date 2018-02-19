f16fa98f23464af7437b905eb674892a2cb07aa9
Raimondo Caraviello <raicaraviello2@gmail.com>
2018-02-19 12:35:35 +0100

    aggiunto somma 20 e somma 100

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -14,6 +14,23 @@ public class Sommatore
 			return x+10;
 		
 	}
+	
+	public static int somma20(int x) 
+	{
+		for (int i = 0; i < 20; i++) 
+			x++;
+		
+			return x;
+		
+	}
+	public static int somma100(int x) 
+	{
+		for (int i = 0; i < 100; i++) 
+			x++;
+		
+			return x;
+		
+	}
 
 
 	
@@ -23,6 +40,8 @@ public class Sommatore
 		int x=1;
 		System.out.println(somma4(x));
 		System.out.println(somma10(x));
+		System.out.println(somma20(x));
+		System.out.println(somma100(x));
 		
 	}
 
