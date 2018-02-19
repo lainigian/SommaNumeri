b459b2ab8dc5210c616cdbbd534000ee4a02b110
Raimondo Caraviello <raicaraviello2@gmail.com>
2018-02-19 12:12:30 +0100

    aggiunto somma 10"

--- src/Sommatore.java
+++ src/Sommatore.java
@@ -10,6 +10,15 @@ public class Sommatore
 			return x;
 		
 	}
+	public static int somma10(int x) 
+	{
+		for (int i = 0; i < 10; i++) 
+		
+			x++;
+			return x;
+		
+	}
+
 
 	
 	public static void main(String[] args) 
@@ -17,6 +26,8 @@ public class Sommatore
 		// TODO Auto-generated method stub
 		int x=1;
 		System.out.println(somma4(x));
+		System.out.println(somma10(x));
+		
 	}
 
 }
