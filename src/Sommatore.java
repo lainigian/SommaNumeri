ce907618d480bf56f8521a3279420da0a49ce126
Raimondo Caraviello <raicaraviello2@gmail.com>
2018-02-19 12:08:26 +0100

    aggiunto somma4

---
+++ src/Sommatore.java
@@ -0,0 +1,22 @@
+
+public class Sommatore
+{
+
+	public static int somma4(int x) 
+	{
+		for (int i = 0; i < 4; i++) 
+		
+			x++;
+			return x;
+		
+	}
+
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int x=1;
+		System.out.println(somma4(x));
+	}
+
+}
